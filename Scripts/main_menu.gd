c5f77827a139a41d26bfd22ef2cf20c0dfcde024
tkellett <thessalicon@gmail.com>
2025-03-04 15:38:05 +0000

    final final 2

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -9,6 +9,13 @@ func _on_play_button_pressed():
 func _on_settings_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/settings.tscn")
 
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.pressed:  # Check if the button is pressed (not released)
+			if event.button_index == MOUSE_BUTTON_LEFT:  # Left mouse button
+				get_tree().change_scene_to_file("res://Scenes/main.tscn")
+			elif event.button_index == MOUSE_BUTTON_RIGHT:  # Right mouse button
+				print("Right mouse button clicked at:", event.position)
 
 func _on_exit_button_pressed():
 	get_tree().quit()
